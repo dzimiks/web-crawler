f5dd73a5a5d4c4123f616d07db7e1fa9b63ee7e4
dzimiks <vana997@gmail.com>
2018-02-07 00:48:04 +0100

    Add functions for dealing with files

---
+++ general.py
@@ -0,0 +1,26 @@
+import os
+
+# Each website you crawl is a separate project (folder)
+def create_project_dir(directory):
+    if not os.path.exists(directory):
+        print('Creating project ' + directory)
+        os.makedirs(directory)
+
+# Create queue and crawled files
+def create_data_files(project_name, base_url):
+    queue = project_name + '/queue.txt'
+    crawled = project_name + '/crawled.txt'
+
+    if not os.path.isfile(queue):
+        write_file(queue, base_url)
+
+    if not os.path.isfile(crawled):
+        write_file(crawled, '')
+
+# Create a new file
+def write_file(path, data):
+    f = open(path, 'w')
+    f.write(data)
+    f.close()
+
+create_data_files('dzimiks', 'https://github.com/dzimiks')
