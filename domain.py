80b4d2cef1ec98ee97440d330feae6a344a12250
dzimiks <vana997@gmail.com>
2018-02-07 02:07:03 +0100

    Create workers and domain checker

---
+++ domain.py
@@ -0,0 +1,18 @@
+from urllib.parse import urlparse
+
+
+# Get domain name
+def get_domain_name(url):
+    try:
+        results = get_subdomain_name(url).split('.')
+        return results[-2] + '.' + results[-1]
+    except:
+        return ''
+
+
+# Get subdomain name
+def get_subdomain_name(url):
+    try:
+        return urlparse(url).netloc
+    except:
+        return ''
