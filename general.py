a250be71da2a9f8115d7d8e429acf0cfcb5b6ae2
dzimiks <vana997@gmail.com>
2018-02-07 01:36:34 +0100

    Add spider

--- general.py
+++ general.py
@@ -1,10 +1,12 @@
 import os
 
+
 # Each website you crawl is a separate project (folder)
 def create_project_dir(directory):
     if not os.path.exists(directory):
         print('Creating project ' + directory)
         os.makedirs(directory)
+
 
 # Create queue and crawled files
 def create_data_files(project_name, base_url):
@@ -17,21 +19,25 @@ def create_data_files(project_name, base_url):
     if not os.path.isfile(crawled):
         write_file(crawled, '')
 
+
 # Create a new file
 def write_file(path, data):
     f = open(path, 'w')
     f.write(data)
     f.close()
 
+
 # Add data onto an existing file
 def append_to_file(path, data):
     with open(path, 'a') as file:
         file.write(data + '\n')
 
+
 # Delete the contents of a file
 def delete_file_contents(path):
     with open(path, 'w'):
         pass
+
 
 # Read a file and convert each line to set items
 def file_to_set(file_name):
@@ -42,6 +48,7 @@ def file_to_set(file_name):
             results.add(line.replace('\n', ''))
 
     return results
+
 
 # Iterate through a set, each item will be a new line in the file
 def set_to_file(links, file):
--- link_finder.py
+++ link_finder.py
@@ -1,6 +1,7 @@
 from html.parser import HTMLParser
 from urllib import parse
 
+
 class LinkFinder(HTMLParser):
 
     def __init__(self, base_url, page_url):
